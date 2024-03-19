5620d49c880b3dfa6a8cea4896f4d229a7046889
AllThaf <thafafadillah@gmail.com>
2024-03-19 19:18:03 +0700

    feat: add register
    Basic register, still required advance logic

--- auth/global_def.py
+++ auth/global_def.py
@@ -1,5 +1,26 @@
-class User():
-    username: str 
+class User:
+    name: str
+    username: str
     password: str
+    fileName: str
 
-    
+    def __innit__(self, name: str, username: str, password: str, fileName: str):
+        self.name = name
+        self.username = username
+        self.password = password
+        self.fileName = fileName
+
+    def register():
+        accFile = open("dataAkun.bin", "a")
+        if accFile == None:
+            print("File tidak dapat dibuka")
+        print("==================REGISTER==================")
+        name = input("Masukkan nama anda: ")
+        username = input("Masukkan username anda: ")
+        password = input("Masukkan password anda: ")
+        fileName = username + ".txt"
+
+        accFile.write(name + " ")
+        accFile.write(username + " ")
+        accFile.write(password + " ")
+        accFile.write(fileName + " ")
